0dadcbefb3a5d1d8daa5b9ee0e20b978ad427893
RakhmatullohR <rrr.engineer.94@gmail.com>
2023-05-19 11:16:00 +0500

    the master branch was created

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('hellow typescript');
+console.log('hellow world');
+// console.log(true + 10);
+throw new Error('Some error');
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log('hellow typescript');
+console.log('hellow world');
+// console.log(true + 10);
+
+throw new Error('Some error');
