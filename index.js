5aaabc54fa468f9e4560e6e1308760cc73448266
RakhmatullohR <rrr.engineer.94@gmail.com>
2023-05-25 11:21:22 +0500

    class_05

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 "use strict";
-console.log('hellow typescript');
-console.log('hellow world');
-// console.log(true + 10);
-throw new Error('Some error');
+// Typescriptda Arrays, tuples & Enums
+// ======= arraylarni e'lon qilish
+let a = [];
+a = [12, 'sdf', true, undefined, {}];
+let b = [1, 2, 3, 4, 5];
+let c = [1, 2, 3, 4, 5];
+let d = [1, '2', 3, 4, '5'];
+let e = [1, 2, true, 4, false];
+// ============== tuples - kartejlar ===========================
+let f;
+// f = ['str', 12, [1, 2, 3]];
+f = [12, 'str', [1, 2, 3]];
+// ============== Enums - qayta hisovlash =============
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+})(Gender || (Gender = {}));
+var G;
+(function (G) {
+    G[G["A"] = 0] = "A";
+    G["B"] = "hellow";
+})(G || (G = {}));
+console.log(Gender);
+console.log(Gender[0], Gender[1], Gender[Gender.Male], Gender[Gender.Female]);
+console.log(G.B);
+console.log(10 /* I.A */, "BBBB" /* I.B */);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,35 @@
-console.log('hellow typescript');
-console.log('hellow world');
-// console.log(true + 10);
+// Typescriptda Arrays, tuples & Enums
 
-throw new Error('Some error');
+// ======= arraylarni e'lon qilish
+let a = [];
+a = [12, 'sdf', true, undefined, {}];
+
+let b: number[] = [1, 2, 3, 4, 5];
+let c: Array<number> = [1, 2, 3, 4, 5];
+
+let d: (number | string)[] = [1, '2', 3, 4, '5'];
+let e: Array<number | boolean> = [1, 2, true, 4, false];
+
+// ============== tuples - kartejlar ===========================
+let f: [number, string, number[]];
+// f = ['str', 12, [1, 2, 3]];
+f = [12, 'str', [1, 2, 3]];
+
+// ============== Enums - qayta hisovlash =============
+enum Gender {
+  Male,
+  Female,
+}
+enum G {
+  A,
+  B = 'hellow',
+}
+console.log(Gender);
+console.log(Gender[0], Gender[1], Gender[Gender.Male], Gender[Gender.Female]);
+console.log(G.B);
+
+const enum I {
+  A = 10,
+  B = 'BBBB',
+}
+console.log(I.A, I.B);
