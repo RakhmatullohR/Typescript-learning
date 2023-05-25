ecea0b6f910a2b50b9e469e9d2a6e99450e5353f
RakhmatullohR <rrr.engineer.94@gmail.com>
2023-05-25 15:36:43 +0500

    class_06

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 "use strict";
-console.log('hellow typescript');
-console.log('hellow world');
-// console.log(true + 10);
-throw new Error('Some error');
+// typescriptda classlar
+class Person {
+    // 1ta klasda constructor bir marta ishlatiladi
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    ifno() {
+        return `His name is ${this._name}. He is ${this._age} years old`;
+    }
+    static description() {
+        return `Bu class haqida ma'lumot beradi...`;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        if (value > 0 && value <= 100) {
+            this._age = value;
+        }
+        else {
+            throw new Error(`Siz noto'g'ri yosh kiritdingiz`);
+        }
+    }
+}
+Person.isFlying = false;
+const rustam = new Person('Rustam', 72);
+// console.log(rustam.ifno());
+// console.log(Person.isFlying);
+// console.log(Person.description());
+console.log(rustam);
+rustam.name = 'Oston';
+rustam.age = 80;
+console.log(rustam);
+if (rustam instanceof Person) {
+    console.log('tegishli');
+}
+else {
+    console.log('tegishli emas');
+}
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,50 @@
-console.log('hellow typescript');
-console.log('hellow world');
-// console.log(true + 10);
+// typescriptda classlar
 
-throw new Error('Some error');
+class Person {
+  _name: string;
+  _age: number;
+  // 1ta klasda constructor bir marta ishlatiladi
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+  ifno(): string {
+    return `His name is ${this._name}. He is ${this._age} years old`;
+  }
+  static isFlying = false;
+  static description(): string {
+    return `Bu class haqida ma'lumot beradi...`;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  set name(value: string) {
+    this._name = value;
+  }
+  get age(): number {
+    return this._age;
+  }
+  set age(value: number) {
+    if (value > 0 && value <= 100) {
+      this._age = value;
+    } else {
+      throw new Error(`Siz noto'g'ri yosh kiritdingiz`);
+    }
+  }
+}
+
+const rustam: Person = new Person('Rustam', 72);
+// console.log(rustam.ifno());
+// console.log(Person.isFlying);
+// console.log(Person.description());
+console.log(rustam);
+rustam.name = 'Oston';
+rustam.age = 80;
+console.log(rustam);
+
+if (rustam instanceof Person) {
+  console.log('tegishli');
+} else {
+  console.log('tegishli emas');
+}
